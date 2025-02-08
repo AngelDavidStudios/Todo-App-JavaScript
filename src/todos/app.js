b41d3fdc413a58fd7a04b4109ae4463412490091
Angel <angeldavidstudios@outlook.com>
2025-02-08 11:40:30 -0500

    Incorporado el html

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,3 +1,4 @@
+import html from './app.html?raw ';
 /**
  *
  * @param { string } elementId
@@ -9,7 +10,7 @@ export const App = ( elementId ) => {
     // Cuando la funcion (App) se llama
     (() => {
         const app = document.createElement('div');
-        app.innerHTML = '<h1>Hola Mundo</h1>';
+        app.innerHTML = html;
         document.querySelector(elementId).append( app )
     })();
 }
